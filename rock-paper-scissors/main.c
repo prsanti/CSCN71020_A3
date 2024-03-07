a96ff4e4d14c757f4ac725d6ca0d23a3810fe669
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-03-06 20:04:38 -0500

    add rps game source code

--- rock-paper-scissors/main.c
+++ rock-paper-scissors/main.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-//#define MAX_CHAR_LIMIT	99
+bool checkIfValidMove(char* move) {
+	bool isValid = true;
+
+	if (strcmp(move, "rock") != 0 && strcmp(move, "paper") != 0 && strcmp(move, "scissors") != 0) {
+		isValid = false;
+	}
+
+	return isValid;
+}
+
+char* game(char* player1, char* player2) {
+	char* gameResult = "";
+
+	// invalid: compares if either player does not input rock, paper or scissors
+	if (checkIfValidMove(player1) == 0 || checkIfValidMove(player2) == 0) {
+		gameResult = "Invalid";
+	} // player 1 wins
+	else if (strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0) {
+		gameResult = "Player1";
+	}
+	else if (strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0) {
+		gameResult = "Player1";
+	}
+	else if (strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0) {
+		gameResult = "Player1";
+	} // player 2 wins
+	else if (strcmp(player1, "rock") == 0 && strcmp(player2, "paper") == 0) {
+		gameResult = "Player2";
+	}
+	else if (strcmp(player1, "paper") == 0 && strcmp(player2, "scissors") == 0) {
+		gameResult = "Player2";
+	}
+	else if (strcmp(player1, "scissors") == 0 && strcmp(player2, "rock") == 0) {
+		gameResult = "Player2";
+	} // draw
+	else if (strcmp(player1, player2) == 0) {
+		gameResult = "Draw";
+	}
+
+	return gameResult;
+}
 
 int main(void) {
-
 	return 0;
 }
 
-char* game(char* playerOne, char* playerTwo) {
-	char* test = "testing";
-	return test;
-}
